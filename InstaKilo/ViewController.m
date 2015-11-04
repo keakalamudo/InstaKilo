2945a11b73c53d19d94a4f9443554c75119669cc
keakalamudo <kelo.akalamudo@gmail.com>
2015-11-04 12:55:18 -0800

    Images are now displayed in a grid.

--- InstaKilo/ViewController.m
+++ InstaKilo/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSArray *imageArray;
+}
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
@@ -16,12 +20,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.\
+    
+    imageArray = [NSArray arrayWithObjects:@"IMG_2307.PNG",@"IMG_2314.JPG",@"IMG_2318.JPG",@"IMG_2323.JPG",@"IMG_2319.JPG",@"IMG_2317.JPG",@"IMG_2320.JPG",@"IMG_2327.JPG",@"IMG_2333.JPG",@"IMG_2342.JPG", nil];
+//                  rrayWithObjects:@"IMG_2307.PNG","IMG_2314.JPG","IMG_2318.JPG","IMG_2323.JPG","IMG_2319.JPG","IMG_2317.JPG","IMG_2320.JPG","IMG_2327.JPG","IMG_2331-1.JPG","IMG_2342.JPG", nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//Sets the number of the items in section to number of images in the image array.
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return imageArray.count;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *identifier = @"cell";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    UIImageView *cellImageView = (UIImageView *)[cell viewWithTag:100];
+    cellImageView.image = [UIImage imageNamed:[imageArray objectAtIndex:indexPath.row]];
+    
+    return cell;
 }
 
 @end
