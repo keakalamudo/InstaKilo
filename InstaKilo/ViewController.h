04a75eed299c367878b6171378f2aa42ac4a7171
keakalamudo <kelo.akalamudo@gmail.com>
2015-11-04 10:26:32 -0800

    Empty Project

---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Kelo Akalamudo on 11/4/15.
+//  Copyright © 2015 Kelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstaKilo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InstaKilo
+//
+//  Created by Kelo Akalamudo on 11/4/15.
+//  Copyright © 2015 Kelo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
